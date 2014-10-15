ea87464c9a162a79917ae66d977d0afcce14999d
Florian Chlan <florianchlan@punktorg.org>
2014-10-15 09:42:50 +0200

    RequestListener implementiert

--- src/main/java/node/ComputationRequestListener.java
+++ src/main/java/node/ComputationRequestListener.java
@@ -1,9 +1,12 @@
 package node;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import javax.print.CancelablePrintJob;
 
 import util.Channel;
 import util.ComputationChannel;
@@ -13,9 +16,9 @@ import util.TcpChannel;
 public class ComputationRequestListener extends Thread {
 
 	private Config config;
-	private ServerSocket serverSocket;
+	private ServerSocket serverSocket = null;
 	private ExecutorService threadPool;
-	
+
 	public ComputationRequestListener(Config config) {
 		this.config = config;
 		openServerSocket();
@@ -23,20 +26,33 @@ public class ComputationRequestListener extends Thread {
 	}
 
 	private void openServerSocket() {
-		serverSocket = new ServerSocket(config.getInt("tcp.port")); // TODO: Exceptions abfangen
+		try {
+			serverSocket = new ServerSocket(config.getInt("tcp.port"));
+		} catch (IOException e) {
+			System.out.println("Couldn't create ServerSocket: " + e.getMessage());
+		} 
 	}
 
 	private void createThreadPool() {
 		threadPool = Executors.newCachedThreadPool();
-		
 	}
 
 	@Override
 	public void run() {
-		while(true) {
-			Socket socket = null;
-			Channel nextRequest = new TcpChannel(serverSocket.accept()); // TODO: Exceptions abfangen
-			threadPool.execute(new SingleRequestHandler(nextRequest, config));
+		if (serverSocket != null) {
+			
+			while (true) {
+				Socket socket = null;
+				Channel nextRequest;
+				try {
+					nextRequest = new TcpChannel(serverSocket.accept());
+					threadPool.execute(new SingleRequestHandler(nextRequest, config));
+
+				} catch (IOException e) {
+					System.out.println("IOException occured: " + e.getMessage());
+					break;
+				}
+			}
 		}
 
 	}
